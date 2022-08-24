072b5fd98abfa3d66e2f73ab751eaf445fe9fd59
Voicu Ioan Vladut <77429938+vladvoicux64@users.noreply.github.com>
2022-08-25 01:12:46 +0300

    Added launcher.

---
+++ launcher.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /D D:\jocuri\engine-sim-vlad-s\bin
+start engine-sim-app.exe
+:loop
+echo Press R to restart and C to close launcher and game...
+choice /c RC /n
+if %errorlevel%==1 goto restart
+if %errorlevel%==2 goto end
+:restart
+taskkill /f /IM engine-sim-app.exe
+start engine-sim-app.exe
+cls
+goto loop
+:end
+taskkill /f /IM engine-sim-app.exe
+exit
